c1c1bef0e3f69142f1fcf5d26030afb1a3689783
zepez <zepezaueralex@gmail.com>
2024-05-02 12:21:04 -0400

    🔍️ Proper open graph and twitter images, favicon

--- packages/web/src/app/layout.tsx
+++ packages/web/src/app/layout.tsx
@@ -56,7 +56,7 @@ export const metadata: Metadata = {
     ],
   },
   icons: {
-    icon: "/favicon.ico",
+    icon: "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📹</text></svg>",
   },
   verification: {
     other: {
